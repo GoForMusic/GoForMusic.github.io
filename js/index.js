6e1a2e2eaa64ad90c40ed6a937f1ccc390b9bc31
Adrian <militaruadriancrisitan@gmail.com>
2022-09-12 15:50:55 +0200

    xxx

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ function onClick(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
     var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
+    captionText.innerHTML = $("#mydiv").html('<object data="http://your-website-domain"/>');
+
   }
   
   // Change style of navbar on scroll
